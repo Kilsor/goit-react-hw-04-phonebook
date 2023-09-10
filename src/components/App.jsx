ee35f63b1f7bf7b553347cea958913d9ba16738b
Kilsor <kilsors@gmail.com>
2023-09-10 13:40:48 +0300

    hw4.2.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const getContacts = () => {
+  const savedContacts = localStorage.getItem('contacts');
+  return savedContacts ? JSON.parse(savedContacts) : [];
+};
 
-  componentDidMount() {
-    // Завантаження контактів з локального сховища при першому відкритті застосунку
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts !== null) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
-    }
-  }
+export const App = () => {
+  // Створення стану для контактів та фільтрації
+  const [contacts, setContacts] = useState(getContacts);
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    // Збереження контактів в локальному сховищі при зміні стану contacts
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  // Ефект для збереження контактів у локальному сховищі при їх зміні
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  handleAddContact = (name, number) => {
-    const isDuplicateName = this.state.contacts.some(
+  // Функція для додавання нового контакту
+  const handleAddContact = (name, number) => {
+    // Перевірка на дублікат імені контакту
+    const isDuplicateName = contacts.some(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
 
@@ -40,38 +31,38 @@ export class App extends Component {
       return;
     }
 
+    // Створення нового контакту і оновлення стану контактів
     const contact = { id: nanoid(), name, number };
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
+    setContacts([...contacts, contact]);
   };
 
-  handleDeleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  // Функція для видалення контакту
+  const handleDeleteContact = contactId => {
+    // Оновлення стану контактів шляхом фільтрації за ідентифікатором
+    setContacts(contacts.filter(contact => contact.id !== contactId));
   };
 
-  handleFilterChange = event => {
-    this.setState({ filter: event.target.value });
+  // Функція для обробки зміни фільтра
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
   };
 
-  render() {
-    const filteredContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
-    );
+  // Фільтрація контактів на основі введеного фільтра
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onAddContact={this.handleAddContact} />
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.handleFilterChange} />
-        <ContactList
-          contacts={filteredContacts}
-          onDeleteContact={this.handleDeleteContact}
-        />
-      </div>
-    );
-  }
-}
+  // Рендер компонентів
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,48 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FormWrapper } from './ContactForm.styled';
 
-export class ContactForm extends Component {
-  state = {
+// Функціональний компонент ContactForm, що приймає `onAddContact` функцію як пропс
+export const ContactForm = ({ onAddContact }) => {
+  // Створення стану для управління формою додавання контакту
+  const [formData, setFormData] = useState({
     name: '',
     number: '',
-  };
+  });
 
-  handleChange = event => {
+  // Обробник зміни вмісту полів вводу форми
+  const handleChange = event => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    setFormData({ ...formData, [name]: value });
   };
 
-  handleSubmit = event => {
+  // Обробник подання форми
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.onAddContact(this.state.name, this.state.number);
-    this.setState({ name: '', number: '' });
+    // Виклик функції onAddContact для додавання нового контакту і очищення форми
+    onAddContact(formData.name, formData.number);
+    setFormData({ name: '', number: '' });
   };
 
-  render() {
-    return (
-      <FormWrapper onSubmit={this.handleSubmit}>
-        <label>Name:</label>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces."
-          required
-          value={this.state.name}
-          onChange={this.handleChange}
-        />
-        <label>Phone number:</label>
-        <input
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          value={this.state.number}
-          onChange={this.handleChange}
-        />
-        <button type="submit">Add Contact</button>
-      </FormWrapper>
-    );
-  }
-}
+  // Рендер форми додавання контакту
+  return (
+    <FormWrapper onSubmit={handleSubmit}>
+      <label>Name:</label>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces."
+        required
+        value={formData.name}
+        onChange={handleChange}
+      />
+      <label>Phone number:</label>
+      <input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={formData.number}
+        onChange={handleChange}
+      />
+      <button type="submit">Add Contact</button>
+    </FormWrapper>
+  );
+};
